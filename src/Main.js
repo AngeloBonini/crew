01f2cc284419db2697d06fd0b7fd17d2eb2eddf1
git <abonini2000@gmail.com>
2020-10-17 21:05:33 -0300

    feat(Table): change table cells and design

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+export const Datas = () => {
+    return (
+    <HomePageHeader></HomePageHeader>
+  );
+};
+
+const HomePageHeader = () => {
+    return (
+      <header className="header">
+        <h2>People Absences</h2>
+      </header>
+    );
+  };
+ 
+  
